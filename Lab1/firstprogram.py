39de48547115ec6d544c4a1c0e43f113395f1ff4
Adele Harrington <adele.harrington@gmail.com>
2021-01-20 17:03:38 +0000

    Modified firstprogram.py to include a second module os, function listdir then executed both listdir and subprocess run, printed their output to view the difference

--- Lab1/firstprogram.py
+++ Lab1/firstprogram.py
@@ -8,13 +8,21 @@
 # I am using W3schools to learn python syntax. I learned about the subprocess module from pymotw.com
 # I tested this program by typing python3 firstprogram.py on my command line
 # I am not sure yet how Anaconda is used with Python
+# I've realised now that obviously running a subprocess means I have assumed the OS will run an ls command
 
 from datetime import date
 
 today = date.today()
 print("I wrote my first python script:", today)
 
-import subprocess
+# Using module OS to get a directory listing
+# I used listdir as opposed to walk because walk required a path but listdir lists the contents of the current directoy
+import os
+CurrentDirListing = os.listdir()
+print("Printing the results of os.listdir command")
+print(CurrentDirListing)
 
+import subprocess
+print("Executing subprocess command run with parameters ls -alrt")
 commandreturncode = subprocess.run(['ls', '-alrt'])
 print('returncode:', commandreturncode.returncode)
