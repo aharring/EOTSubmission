50e6fc38d346daf5f53714a9f30b745ac1896586
Adele Harrington <adele.harrington@gmail.com>
2021-01-31 20:49:53 +0000

    Instructions 12&13 of FirstPrograms. Created hello2.py, prompts for user name, then says hello name nice to meet you

---
+++ week02/hello2.py
@@ -0,0 +1,13 @@
+# This program is based on Week 2 Labs  for programming for CyberSecurity, FirstPrograms.pdf 
+# The labs were set by lecturer Adrew Beatty, GMIT
+# This program satisfies steps 12 & 13 of FirstPrograms.pdf
+#
+# 12. Create a new file called hello2.py that reads in a person’s name and prints out
+# hello that persons name
+# 13. Modify to say nice to meet you after saying hello
+#
+
+# Prompt the user for their name
+name = input("Please enter your name :")
+print ("Hello {}\nNice to meet you!" .format(name))  # Without the space after the Hello the output would read Helloname where name is the input the user types
+
--- week02/multiply.py
+++ week02/multiply.py
@@ -4,6 +4,10 @@
 #
 #	10. Write a program called multiply.py that outputs what 111 * 555 is
 #	
+#	11. Why does this print out nothing, when run
+#		Originally the program just creates the variable answer and assigns it the product of 111*555 then ceases execution
+#		The answer must be expressly sent to stdout/printed
 
-answer = 111*555
+answer = 111*555 # Point 10 of FirstPrograms
+print (answer)	 # Point 11 of FirstPrograms
 
