75b7b7dc5fab5b72de99da7aa41838e1349c3600
Adele Harrington <adele.harrington@gmail.com>
2021-01-31 20:32:54 +0000

    Steps 6-11 in FirstPrograms.pdf, mods to hello.py & multiply.py created with first task of creating variable

--- week02/hello.py
+++ week02/hello.py
@@ -5,5 +5,10 @@
 #	3. Create a directory called week02 (Done one command line, not in program)
 #	4. In week02 create a file called hello.py that just says hello
 #	5. Save the file (CTRL+S) and run it 
+# Modified as follows
+#	6. Change the code to say hello your name 
+#	7. Don’t save it and see that when you run it, the output is still hello
+#	8. Save it and see that the output has changed.
+#	9. Make a mistake with the syntax and try to run it. - for this I removed the quotes so hello became an undefined variable instead of a string
 
-print ("hello")
+print ("hello Adele")
---
+++ week02/multiply.py
@@ -0,0 +1,9 @@
+# This program is based on Week 2 Labs  for programming for CyberSecurity, FirstPrograms.pdf 
+# The labs were set by lecturer Adrew Beatty, GMIT
+# This program satisfies steps 10 & 11 of FirstPrograms.pdf
+#
+#	10. Write a program called multiply.py that outputs what 111 * 555 is
+#	
+
+answer = 111*555
+
