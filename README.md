94615d1b8a3d9fecb30fc8fca0fc9c8746150f82
Adele Harrington <adele.harrington@gmail.com>
2021-11-12 16:28:40 +0000

    Adjusted logging to output suspect findings to specific file, second more verbose file with everything. Also adjusted format for messages are multicoloured. Still more work to do in logging but seems to reliably work with colours now

--- README.md
+++ README.md
@@ -22,6 +22,8 @@
 #       3. https://docs.python.org/3/library/shutil.html
 #       4. https://www.programiz.com/python-programming/regex 
 #       5. https://stackoverflow.com/questions/9617336/how-to-resolve-git-did-not-exit-cleanly-exit-code-128-error-on-tortoisegit
+#       6. https://stackoverflow.com/questions/287871/how-to-print-colored-text-to-the-terminal
+#       7. https://docs.python.org/3/library/logging.html 
+#       8. https://stackoverflow.com/questions/11232230/logging-to-two-files-with-different-settings
 #
-# Next phase : Multiple log files
 
--- idpghvul.py
+++ idpghvul.py
@@ -17,6 +17,7 @@
 #       3. https://docs.python.org/3/library/shutil.html
 #       4. https://www.programiz.com/python-programming/regex 
 #       5. https://stackoverflow.com/questions/9617336/how-to-resolve-git-did-not-exit-cleanly-exit-code-128-error-on-tortoisegit
+#       6. https://docs.python.org/3/library/logging.html
 #
 
 import re       
@@ -29,22 +30,48 @@ import logging # Term #1, Lecture 3, week 9
 from git import Repo
 from git import NULL_TREE
 
-# Redirecting Debug to output idpghvul.log
-DEBUG_LOG_FILE = "idpghvul.log" # Just a straightforward log file, no timestamp, overwritten for each run
+# The output from this program is very long/verbose
+# The output will be split in to two files, one is purely informational & contains everything. The other contains details of suspicious entries only
+# Ideally the informational messages will be streamed to stdout as well so that it is possible to see progress
+ 
+debugLogFile = "idpghvul.log" # Just a straightforward log file, no timestamp, overwritten for each run
+infoLogFile = "idpghvul.info" # This file will contain details of all repositories/files listed in commits as changed, as well as verification of keywords scanned
+pVulLogFile = "idpghvul.pvul"
 
-fileHandler = logging.FileHandler("{0}".format(DEBUG_LOG_FILE))
-rootLogger = logging.getLogger()
-rootLogger.addHandler(fileHandler)
-rootLogger.setLevel(logging.DEBUG)             # Debug Level
+# Old logging method - leaving it here until I'm happy with new method
+#fileHandler = logging.FileHandler("{0}".format(DEBUG_LOG_FILE))
+#rootLogger = logging.getLogger()
+#rootLogger.addHandler(fileHandler)
+#rootLogger.setLevel(logging.DEBUG)             # Debug Level
+
+def configLogFile(name, log_file, level):
+
+    handler = logging.FileHandler(log_file)        
+
+    logger = logging.getLogger(name)
+    logger.setLevel(level)
+    logger.addHandler(handler)
+
+    return logger
+
+# Set up the log file that contains all execution output 
+infoLog = configLogFile('Execution Output', infoLogFile, "INFO")
+infoLog.info('This file contains complete execution output') 
+
+# Set up the log file that contains details of suspicious keywords files found
+pVulLog = configLogFile('Potential Vulnerabilities', pVulLogFile, "WARNING")
+pVulLog.warning('This file contains suspect findings \n')
+
 
 # Hardcoded for now.
 users = ["andrewbeattycourseware"] # Just me for now
 ghUrl = "https://api.github.com"
 
 class colors:
-    FILENAME = "\033[1m"
+    FILENAME = "\033[1;33m" # File name Bold Red
+    COMMIT = "\033[1;36m"   # Commit name Bold Light Blue
     NORMAL = "\033[0m"
-    WARNING = "\033[93m"
+    WARNING = "\033[1;31m"  # WARNING keyword - Bold Red
 
 # Keywords that might indicate vulnerabilities in a repository
 likelyCandidates = {
@@ -84,36 +111,53 @@ def retrieveRepos ():
 # Retrieving repos based on username. Not concerned about orgs or contributors yet 
     for user in users:
         repoPath = "{}/users/{}/repos".format(ghUrl, user)
-        logging.debug("Repo Path\n%s", repoPath)   # Logging repo path being reviewed
+        #logging.debug("Repo Path\n%s", repoPath)                  # Logging repo path being reviewed
         listRepos = requestRP(repoPath)
     for repo in listRepos:
         if repo["fork"] == False :                                # If the repository is not a fork
-            logging.info("Repo details\n%s", repo["git_url"])     # Logging repo path being reviewed
+            infoLog.info("Repo details\n%s", repo["git_url"])
+           #logging.info("Repo details\n%s", repo["git_url"])     # Logging repo path being reviewed
            #  print (repo)
             healthCheck(repo["git_url"])
 
 def findPossibleProblems (commitDiff):
 
-    logging.info("Checking {}{}{}\n".format(colors.FILENAME, commitDiff.b_path, colors.NORMAL)) 
-    print("{}{}{}".format(colors.FILENAME, commitDiff.b_path, colors.NORMAL))
+    #logging.info("Checking {}{}{}\n".format(colors.FILENAME, commitDiff.b_path, colors.NORMAL)) # Old logging, will remove once all working ok
+    infoLog.info("{}Checking {}{}{}" .format(colors.NORMAL, colors.FILENAME, commitDiff.b_path, colors.NORMAL))                    # Haven't quite figured out coloring text yet
+    print("{}Checking {}{}{}".format(colors.NORMAL, colors.FILENAME, commitDiff.b_path, colors.NORMAL)) # Std Out - but ideally I don't want to have to write a separate line here
+
     blob_text = commitDiff.diff.decode("utf-8", errors="replace")
     for suspectPhrase in likelyCandidates:
         if re.search(suspectPhrase, blob_text, re.IGNORECASE):
-            print(
-                "Found suspectPhrase {}{}{} in this file. You should check if there are {}{} exposed.".format(
+            pVulLog.warning (
+                "{}Suspect phrase {} {} {} found in {} {} {}".format(
+                    colors.NORMAL,
                     colors.WARNING,
                     suspectPhrase,
                     colors.NORMAL,
-                    colors.WARNING,
+                    colors.COMMIT,
+                    commitDiff.b_path,
+                    colors.NORMAL
+                )
+            )
+            print(
+                "{}Suspect phrase {} {} {} found in {} {} {}".format(
                     colors.NORMAL,
+                    colors.WARNING,
+                    suspectPhrase,
+                    colors.NORMAL,
+                    colors.COMMIT,
+                    commitDiff.b_path,
+                    colors.NORMAL
                 )
             )
         else :
-            logging.info("Suspect Phrase {} not found" .format(suspectPhrase)) 
+            infoLog.info("Suspect Phrase {} not found" .format(suspectPhrase))
+            #logging.info("Suspect Phrase {} not found" .format(suspectPhrase)) 
 
 def healthCheck(repoUrl):
 
-    logging.debug("In healthCheck : Param Repo Url = %s", repoUrl) 
+    #logging.debug("In healthCheck : Param Repo Url = %s", repoUrl) 
 
     tmpDir = cloneRepo(repoUrl)
     repo = Repo(tmpDir)
@@ -122,20 +166,22 @@ def healthCheck(repoUrl):
     # print (branches)        Just actually checking I've got something
     for branch in branches:
         branchName = branch.name
-        logging.info(branchName)
+        infoLog.info(branchName)
+        #logging.info(branchName)
         print(branchName)
         for commit in repo.iter_commits(branchName, max_count=100):
             print("=" * 25)
 
-            logging.info(
-                "\n{}Searching commit {}.{}".format(
-                    colors.FILENAME, commit.hexsha, colors.NORMAL
+            #logging.info(
+            infoLog.info(
+                "\n{}Searching commit {}{}{}".format(
+                    colors.NORMAL, colors.COMMIT, commit.hexsha, colors.NORMAL
                 )
             )
 
             print(
-                "\n{}Searching commit {}.{}".format(
-                    colors.FILENAME, commit.hexsha, colors.NORMAL
+                "\n{}Searching commit {}{}{}".format(
+                    colors.NORMAL, colors.COMMIT, commit.hexsha, colors.NORMAL
                 )
             )
 
